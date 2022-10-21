9c3baf5cd15d0984d9ce71358b402deeeb096c09
Shawn Price <shawn@bighead.club>
2022-10-20 21:42:07 -0700

    Setup Polygon deploys. Remove husky for netlify

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -3,11 +3,19 @@ const formatJSON = require("../formatJSON");
 
 const fs = require("fs");
 const col = require("../console.colour");
+const { exit } = require("process");
 const log = col.colour;
 
 //TODO: SET A PRICE FOR DEPLOYMENTS
 // const MAINNET_GAS_PRICE = ethers.utils.parseUnits(config.deploy.MAINNET_GAS_PRICE_GWEI,"gwei");
 const PREVENT_MAINNET = true; //config.deploy.PREVENT_MAINNET;
+
+const tokenAddresses = {
+  battle: '',
+  dice: '',
+  token: '',
+  viewer: '',
+}
 
 let addresses = {};
 const addressFile = __dirname + "/../addresses.json";
@@ -39,23 +47,22 @@ async function main() {
   const [deployer] = await ethers.getSigners();
 
   log("Deploying contract with the account:", deployer.address);
-  log("Account balance:", (await deployer.getBalance()).toString());
+  // log("Account balance:", (await deployer.getBalance()).toString());
   log();
 
-  let goerliLimit = false;
+  // let goerliLimit = false;
 
   switch (_network) {
     case "ganache":
-    case "ropsten":
-    case "rinkeby":
     case "hardhat":
+    case "mumbai":
       break;
-    case "goerli":
-      goerliLimit = true;
-      break;
-    case "mainnet":
+    // case "goerli":
+    //   goerliLimit = true;
+    //   break;
+    case "polygon":
       if (PREVENT_MAINNET) {
-        col.red("  MAINNET PREVENTED");
+        col.red("  POLYGON MAINNET PREVENTED");
         process.exit();
       }
       break;
@@ -71,52 +78,68 @@ async function main() {
     config.contracts.token.uriSuffix,
     overrides
   );
-  let tx = await BattleToken.deployed();
-  totalGasUsed += parseInt(tx.deployTransaction.gasLimit);
+  await BattleToken.deployed();
+  // totalGasUsed += parseInt(tx.deployTransaction.gasLimit);
+
+  const battleTokenTxHash = BattleToken.deployTransaction.hash;
+  const battleTokenTxReceipt = await ethers.provider.waitForTransaction(battleTokenTxHash);
 
   col.green("    done.");
-  col.cyan("BattleToken address:", BattleToken.address);
+  col.cyan("BattleToken address:", battleTokenTxReceipt.contractAddress);
+  tokenAddresses.token = battleTokenTxReceipt.contractAddress;
   log();
 
   col.magenta("Deploying BattleDice...");
   overrides = {};
   const BattleDiceFactory = await ethers.getContractFactory("BattleDice");
   const BattleDice = await BattleDiceFactory.deploy(overrides);
-  tx = await BattleDice.deployed();
-  totalGasUsed += parseInt(tx.deployTransaction.gasLimit);
+  await BattleDice.deployed();
+  // totalGasUsed += parseInt(tx.deployTransaction.gasLimit);
+
+  const diceTxHash = BattleDice.deployTransaction.hash;
+  const diceTxReceipt = await ethers.provider.waitForTransaction(diceTxHash);
 
   col.green("    done.");
-  col.cyan("BattleDice address:", BattleDice.address);
+  col.cyan("BattleDice address:", diceTxReceipt.contractAddress);
+  tokenAddresses.dice = diceTxReceipt.contractAddress;
   log();
 
   col.magenta("Deploying Battle...");
   overrides = {};
   const BattleFactory = await ethers.getContractFactory("Battle");
   const Battle = await BattleFactory.deploy(
-    BattleToken.address,
-    BattleDice.address,
+    battleTokenTxReceipt.contractAddress,
+    diceTxReceipt.contractAddress,
     overrides
   );
-  tx = await Battle.deployed();
-  totalGasUsed += parseInt(tx.deployTransaction.gasLimit);
+  await Battle.deployed();
+  // totalGasUsed += parseInt(tx.deployTransaction.gasLimit);
+
+  const battleTxHash = Battle.deployTransaction.hash;
+  const battleTxReceipt = await ethers.provider.waitForTransaction(battleTxHash);
 
   col.green("    done.");
-  col.cyan("Battle address:", Battle.address);
+  col.cyan("Battle address:", battleTxReceipt.contractAddress);
+  tokenAddresses.battle = battleTxReceipt.contractAddress;
   log();
 
   col.magenta("Deploying BattleViewer...");
   overrides = {};
   const BattleViewerFactory = await ethers.getContractFactory("BattleViewer");
   const BattleViewer = await BattleViewerFactory.deploy(
-    Battle.address,
-    BattleToken.address,
+    battleTxReceipt.contractAddress,
+    battleTokenTxReceipt.contractAddress,
     overrides
   );
-  tx = await BattleViewer.deployed();
-  totalGasUsed += parseInt(tx.deployTransaction.gasLimit);
+  await BattleViewer.deployed();
+  // totalGasUsed += parseInt(tx.deployTransaction.gasLimit);
+
+  const viewerTxHash = Battle.deployTransaction.hash;
+  const viewerTxReceipt = await ethers.provider.waitForTransaction(viewerTxHash);
 
   col.green("    done.");
-  col.cyan("BattleViewer address:", BattleViewer.address);
+  col.cyan("BattleViewer address:", viewerTxReceipt.contractAddress);
+  tokenAddresses.viewer = viewerTxReceipt.contractAddress;
   log();
 
   log();
@@ -124,7 +147,7 @@ async function main() {
   log();
   col.magenta("   Connect BattleToken to Battle...");
   try {
-    let tx = await BattleToken.setBattle(Battle.address);
+    let tx = await BattleToken.setBattle(battleTxReceipt.contractAddress);
     await tx.wait();
     col.green("    done.");
   } catch (e) {
@@ -133,14 +156,9 @@ async function main() {
   }
   log();
 
-  addresses[_network] = {
-    battle: Battle.address,
-    dice: BattleDice.address,
-    token: BattleToken.address,
-    viewer: BattleViewer.address,
-  };
+  addresses[_network] = tokenAddresses;
 
-  col.yellow(" TOTAL GAS USED:", totalGasUsed.toString());
+  // col.yellow(" TOTAL GAS USED:", totalGasUsed.toString());
 
   log("   Saving addresses...");
   try {
